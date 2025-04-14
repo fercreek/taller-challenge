824cfab5286b8c273773acd8ca715d7db0979823
Fernando Contreras <fercreek@gmail.com>
2025-04-14 17:42:34 -0600

    Complete the book reserve endpoint

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem "thruster", require: false
 
 group :development, :test do
   gem "rspec-rails", "~> 6.0"
+  gem "pry"
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
 
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Books Spec', type: :request do
+  # Just for the general check
   describe 'GET /books' do
     it 'returns a book list' do
       get '/books'
@@ -15,7 +16,45 @@ RSpec.describe 'Books Spec', type: :request do
       post '/books', params: { book: { title: 'Taller Challenge' } }
 
       expect(response).to have_http_status(:created)
-      expect(JSON.parse(response.body)).to include("title" => "Taller Challenge")
+      expect(JSON.parse(response.body)).to include(
+        "title" => "Taller Challenge"
+        )
     end
+  end
+
+  describe 'POST /books/:id/reserve' do
+    let!(:book) { Book.create(title: 'Taller Challenge') }
+
+    context 'when the books is available' do
+      it 'reserves the book sucessfully' do
+        email = "john@doe.com"
+        post "/books/#{book.id}/reserve",
+              params: { user_email: "john@doe.com" }
+
+        expect(response).to have_http_status(:created)
+        expect(JSON.parse(response.body)).to include(
+          "status" => "reserved",
+          "user_email" => email
+          )
+      end
+    end
+
+    context "when the books is reserved" do
+      before do
+        book.update(status: 'reserved', user_email: 'doe@joe.com')
+      end
+
+      it 'returns an error' do
+        post "/books/#{book.id}/reserve", params: { user_email: "john@doe.com" }
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to include(
+          'error' => "Book is already reserved"
+        )
+      end
+    end
+
+    # context 'when the user_email is not provided' do (not generated for the time)
+    # end
   end
 end
